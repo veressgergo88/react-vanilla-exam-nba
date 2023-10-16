3fce72a096cd79dd46fe5a051032973ca86fe775
veressgergo88 <veress.gergo@gmail.com>
2023-10-16 21:56:41 +0200

    submit button deleted, useMemo used

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,63 +1,68 @@
-import { useEffect, useState } from "react"
-import { loadTeams, type Team, type Player } from "./api"
+import { useEffect, useMemo, useState } from "react";
+import { loadTeams, type Team } from "./api";
 
 export default function App() {
-  const [error, setError] = useState<string | null>(null)
-  const [teams, setTeams] = useState<Team[]>([])
-  const [players, setPlayers] = useState<Player[]>([])
-  const [selectedTeam, setSelectedTeam] = useState("Select a team")
-  const [selectedTeamData, setSelectedTeamData] = useState<Team>()
-  const [isSubmit, setIsSubmit] = useState(false)
-  
+  const [error, setError] = useState<string | null>(null);
+  const [teams, setTeams] = useState<Team[]>([]);
+  const [selectedTeam, setSelectedTeam] = useState("Select a team");
+  const [valamiInput, setValamiInput] = useState("")
+
   const loadTeamsHandler = async () => {
-      const response = await loadTeams()
-      if (!response.success) {
-        setError("Cannot find teams!")
-        } else {
-        setTeams(response.data)
-      }
-  }
-  
+    const response = await loadTeams();
+    if (!response.success) {
+      setError("Cannot find teams!");
+    } else {
+      setTeams(response.data);
+    }
+  };
+
   useEffect(() => {
-    loadTeamsHandler()
-  },[])
+    loadTeamsHandler();
+  }, []);
 
-  const subTeam = () => {
-    const teamData = teams.find((t) => t.name === selectedTeam)
-    setSelectedTeamData(teamData)
-    if(selectedTeamData)
-      setPlayers(selectedTeamData.players)
-    
-    setIsSubmit(true)
-  }
-
+  const teamData = useMemo(() => teams.find((t) => t.name === selectedTeam),[selectedTeam]);
+  
   return (
-    <>
-      <h1>Vote NBA players</h1>
-      <div>
-        <label>Select your favorite player's team
-          <h1>{error}</h1>
-          <select value={selectedTeam} onChange={(e) => setSelectedTeam(e.target.value)}>
-            <option>{selectedTeam}</option>
-            {teams.map((team) => (
-              <option key={team.id} value={team.name}>{team.name}</option>
+    <div className="card w-96 bg-base-100 shadow-xl">
+      <div className="card-body items-center text-center">
+        <div className="card-title">
+          <h1>Vote NBA players</h1>
+        </div>
+        <div>
+          <label>
+            Select your favorite player's team
+            <h1>{error}</h1>
+            <select
+              className="select select-bordered w-full max-w-xs"
+              value={selectedTeam}
+              onChange={(e) => setSelectedTeam(e.target.value)}
+            >
+              <option>{selectedTeam}</option>
+              {teams.map((team) => (
+                <option key={team.id} value={team.name}>
+                  {team.name}
+                </option>
+              ))}
+            </select>
+          </label>
+        </div>
+
+        {teamData?.players.length ? (
+          <ul className="mt-4">
+            {teamData.players.map((player) => (
+              <li
+                className="flex justify-between items-center gap-10 mt-4 mb-4"
+                key={player.id}
+              >
+                <h2 className="text-xl">{player.name}</h2>
+                <button className="btn btn-primary">Vote</button>
+              </li>
             ))}
-          </select>
-        </label>
-      </div>
-      <button onClick={subTeam}>Show the players</button>
-      <div>
-        { isSubmit && (
-        <>
-          {players.map((player) => (
-            <div key={player.id}>
-              <h2>{player.name}</h2>
-              <button>Vote</button>
-            </div>
-          ))}
-        </>
+          </ul>
+        ) : (
+          <h2>There is no players!</h2>
         )}
       </div>
-    </>
-  )
+    </div>
+  );
 }
